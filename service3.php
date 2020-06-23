98f17ceec79b2197beed5b0b2107be8366559608
unknown <richeti1998@gmail.com>
2020-06-22 20:58:17 -0500

    ser

--- service3.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('lib/nusoap.php');
-$client = new nusoap_client('https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl','wsdl');
-$err = $client->getError();
-if ($err) {	echo 'Error en Constructor' . $err ; }
-$param = array('plengua' => 'C','pcurso'=> '2011-12','pcoddep'=>'B142','pcodest'=>'');
-$result1 = $client->call('wsasidepto', $param);
-print_r($result1);
-?>
